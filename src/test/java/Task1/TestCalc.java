e25923df1c4abcde5f296307f4cfdd7b10476f0d
殷畅 <381355732@qq.com>
2021-10-03 19:03:22 +0800

    fail

--- src/test/java/Task1/TestCalc.java
+++ src/test/java/Task1/TestCalc.java
@@ -1,17 +1,18 @@
 package Task1;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestCalc {
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void TestAdd() {
         int a =5;
         int b = 6;
         int c = Calc.add(a,b);
         assertEquals(11,c);
     }
-    @org.junit.jupiter.api.Test
+    @Test
     public void TestSubtract() {
         int a =5;
         int b = 6;
