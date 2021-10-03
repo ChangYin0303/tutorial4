90a5077411c5647e40f3421c5bd82a33e3d3cb6e
殷畅 <381355732@qq.com>
2021-10-03 17:11:34 +0800

    first

---
+++ src/main/java/Task1/Calc.java
@@ -0,0 +1,12 @@
+package Task1;
+
+public class Calc {
+    public static int add(int a, int b){
+        int c = a+b;
+        return c;
+    }
+    public static int subtract(int a, int b){
+        int c = a+b;
+        return c;
+    }
+}
---
+++ src/test/java/Task1/TestCalc.java
@@ -0,0 +1,14 @@
+package Task1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestCalc {
+
+    @org.junit.jupiter.api.Test
+    public void TestAdd() {
+        int a =5;
+        int b = 6;
+        int c = Calc.add(a,b);
+        assertEquals(11,c);
+    }
+}
